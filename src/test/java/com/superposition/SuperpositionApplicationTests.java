1e3ddc3d162785f9b34a9bdcb53d8cc3a3c02e6e
정서현 <tjgus027@kakao.com>
2023-10-23 23:26:49 +0900

    build: initial setting

---
+++ src/test/java/com/superposition/SuperpositionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.superposition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperpositionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
