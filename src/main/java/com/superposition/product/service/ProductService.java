6e31be46a1b2c859a0069fba6381a3eae495a267
정서현 <tjgus027@kakao.com>
2023-11-24 01:01:53 +0900

    Feat: 작가별 작품 정보 조회 구현

--- src/main/java/com/superposition/product/service/ProductService.java
+++ src/main/java/com/superposition/product/service/ProductService.java
@@ -5,10 +5,12 @@ import com.superposition.product.dto.ResponseProductDetail;
 import com.superposition.product.dto.ResponseProduct;
 
 import java.util.List;
+import java.util.Map;
 
 public interface ProductService {
     public List<ResponseProduct> getAllProducts(String search);
     public ResponseProductDetail getProductById(String productId, boolean isQr);
+    public Map<String, String> getProductByName(String name);
     public Payload likeProduct(String productId, boolean isLike);
     public void orderClickCount(String productId);
     public void addView(String productId);
--- src/main/java/com/superposition/product/service/ProductServiceImpl.java
+++ src/main/java/com/superposition/product/service/ProductServiceImpl.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class ProductServiceImpl implements ProductService {
@@ -30,6 +31,11 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public ResponseProductDetail getProductById(String productId, boolean isQr) {
         return toResponseBuild(productId, isQr);
+    }
+
+    @Override
+    public Map<String, String> getProductByName(String name) {
+        return productMapper.getProductByName(name);
     }
 
     @Override
