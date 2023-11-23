e3239ef978297012b03f34723d95d6bed3379997
정서현 <tjgus027@kakao.com>
2023-11-24 01:34:41 +0900

    Refactor: build 메소드 분리

--- src/main/java/com/superposition/artist/service/ArtistServiceImpl.java
+++ src/main/java/com/superposition/artist/service/ArtistServiceImpl.java
@@ -1,19 +1,24 @@
 package com.superposition.artist.service;
 
 import com.superposition.artist.domain.mapper.ArtistMapper;
+import com.superposition.artist.dto.ArtistDto;
 import com.superposition.artist.dto.ResponseArtist;
 import com.superposition.artist.dto.ResponseArtistDetail;
 import com.superposition.artist.dto.ResponseDisplayArtist;
+import com.superposition.product.service.ProductService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class ArtistServiceImpl implements ArtistService{
     private final ArtistMapper artistMapper;
+    private final ProductService productService;
 
-    public ArtistServiceImpl(ArtistMapper artistMapper) {
+    public ArtistServiceImpl(ArtistMapper artistMapper, ProductService productService) {
         this.artistMapper = artistMapper;
+        this.productService = productService;
     }
 
     @Override
@@ -29,10 +34,21 @@ public class ArtistServiceImpl implements ArtistService{
 
     @Override
     public ResponseArtistDetail getArtistByName(String name) {
-        return artistMapper.getArtistByName(name);
+        return dtoToResponse(artistMapper.getArtistByName(name), name);
     }
 
     private List<ResponseArtist> searchByKeyword(String keyword){
         return artistMapper.getArtistByKeyword(keyword);
+    }
+
+    private ResponseArtistDetail dtoToResponse(ArtistDto dto, String name){
+        return ResponseArtistDetail.builder()
+                .profile(dto.getProfile())
+                .name(dto.getName())
+                .introduce(dto.getIntroduce())
+                .isDisplay(dto.isDisplay())
+                .description(dto.getDescription())
+                .products(productService.getProductByName(name))
+                .instaId(dto.getInstagramId()).build();
     }
 }
