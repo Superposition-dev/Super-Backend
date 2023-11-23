09e7a73147273e7e902bdf7f0105068666051093
정서현 <tjgus027@kakao.com>
2023-11-24 00:14:58 +0900

    Feat: 이름으로 작품 데이터(아이디, 이미지) 조회 구현

--- src/main/java/com/superposition/product/domain/mapper/ProductMapper.java
+++ src/main/java/com/superposition/product/domain/mapper/ProductMapper.java
@@ -5,6 +5,7 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface ProductMapper {
@@ -14,6 +15,7 @@ public interface ProductMapper {
     public ProductDto getProductById(@Param("productId") long productId);
     public PictureInfo getPictureInfoById(@Param("productId") long productId);
     public String[] getTagsById(@Param("productId") long productId);
+    public Map<String, String> getProductByName(@Param("name") String name);
     public void addBasicView(@Param("productId") long productId);
     public void addQrView(@Param("productId") long productId);
     public void likeProduct(@Param("productId") long productId);
