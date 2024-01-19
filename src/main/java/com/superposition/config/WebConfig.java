7c4f08c6f33eb8da58622fce87494b7c7696be6b
정서현 <tjgus027@kakao.com>
2024-01-19 14:16:14 +0900

    Refactor: WebConfig 리팩토링

--- src/main/java/com/superposition/config/WebConfig.java
+++ src/main/java/com/superposition/config/WebConfig.java
@@ -1,16 +1,21 @@
-package com.superposition;
+package com.superposition.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    @Value("${cors.pathPattern}")
+    private String pathPattern;
+    @Value("${cors.allowedOrigins}")
+    private String allowedOrigins;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("https://spp-art.vercel.app/", "https://spp-art.com/")
+        registry.addMapping(pathPattern)
+                .allowedOrigins(allowedOrigins, "http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
                 .allowedHeaders("Authorization", "Content-Type")
                 .exposedHeaders("Custom-Header")
