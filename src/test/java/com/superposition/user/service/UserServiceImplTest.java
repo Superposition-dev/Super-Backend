f6dcc2a3eb3bf53652b8746097a106cb20a801f3
정서현 <tjgus027@kakao.com>
2024-01-26 19:59:44 +0900

    Test: 회원 정보 조회 성공 및 실패 케이스 테스트

--- src/test/java/com/superposition/user/service/UserServiceImplTest.java
+++ src/test/java/com/superposition/user/service/UserServiceImplTest.java
@@ -2,8 +2,10 @@ package com.superposition.user.service;
 
 import com.superposition.user.dto.LoginResponse;
 import com.superposition.user.dto.RequestUserInfo;
+import com.superposition.user.exception.EmptyEmailException;
 import com.superposition.utils.Gender;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,23 +17,27 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.Date;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
 class UserServiceImplTest {
     private final UserService userService;
+    private RequestUserInfo userInfo;
 
     @Autowired
     UserServiceImplTest(UserService userService) {
         this.userService = userService;
     }
 
+    @BeforeEach
+    void beforeTest(){
+        userInfo = RequestUserInfo.builder().email("test@gamil.com").name("testName").nickname("test").profile("profile").gender(Gender.F).birthDate(new Date(2024,1, 1)).build();
+    }
+
     @Test
     @DisplayName("회원가입 성공 테스트")
     void 회원가입_테스트() {
-        //given
-        RequestUserInfo userInfo = RequestUserInfo.builder().email("test@gamil.com").name("testName").nickname("test").profile("profile").gender(Gender.F).birthDate(new Date(2024,1, 1)).build();
-
         //when
         LoginResponse signup = userService.signup(userInfo);
 
@@ -43,12 +49,38 @@ class UserServiceImplTest {
     @DisplayName("토큰 재발급 실패 테스트")
     void 토큰_재발급(){
         //given
-        String email = "";
+        String email = "no value";
 
         //when
         ResponseEntity<?> responseEntity = userService.regenerateToken(email);
 
         //then
         assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.UNAUTHORIZED);
+    }
+
+    @Test
+    @DisplayName("회원 정보 조회")
+    void 정보_조회() {
+        //given
+        String email = userService.signup(userInfo).getUserInfo().getEmail();
+
+        //when
+        ResponseEntity<?> info = userService.getUserInfo(email);
+
+        //then
+        assertThat(info.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+
+    @Test
+    @DisplayName("회원 정보 조회 실패 테스트")
+    void 정보_조회2() {
+        //given
+        String email = null;
+
+        //when
+        EmptyEmailException e = assertThrows(EmptyEmailException.class, () -> userService.getUserInfo(email));
+
+        //then
+        assertThat(e.getClass()).isEqualTo(EmptyEmailException.class);
     }
 }
