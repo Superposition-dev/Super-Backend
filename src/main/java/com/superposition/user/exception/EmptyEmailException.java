80ccf0db2a7753025efe85a13a5a105542f070f1
정서현 <tjgus027@kakao.com>
2024-01-30 12:53:21 +0900

    Refactor: 응답 코드 변경 (401 -> 403)

--- src/main/java/com/superposition/user/exception/EmptyEmailException.java
+++ src/main/java/com/superposition/user/exception/EmptyEmailException.java
@@ -3,6 +3,6 @@ package com.superposition.user.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.BAD_REQUEST)
+@ResponseStatus(value = HttpStatus.FORBIDDEN)
 public class EmptyEmailException extends RuntimeException{
 }
