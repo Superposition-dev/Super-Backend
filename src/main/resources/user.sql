4ed01968fbd98ab2e59a4979e182b59e92e4f7a0
harrie8 <hesjs5@naver.com>
2024-02-06 23:23:00 +0900

    Refactor: 불필요한 코드 제거
    following 컬럼 삭제

--- src/main/resources/user.sql
+++ src/main/resources/user.sql
@@ -11,6 +11,5 @@ CREATE TABLE `USER` (
         `create_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
         `nickname_update_at` date DEFAULT NULL,
         `delete_at` timestamp NULL DEFAULT NULL,
-        `is_active` tinyint(1) NOT NULL DEFAULT '1',
-        `following` varchar(100) DEFAULT NULL
+        `is_active` tinyint(1) NOT NULL DEFAULT '1'
 );
