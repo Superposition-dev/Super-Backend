a2553bff9ca03ab33d39e2c508237adfc57fdb92
harrie8 <hesjs5@naver.com>
2024-02-12 10:50:40 +0900

    Fixed: artist_follow 테이블의 created_at 컬럼 삭제

--- src/main/java/com/superposition/artist/domain/entity/ArtistFollow.java
+++ src/main/java/com/superposition/artist/domain/entity/ArtistFollow.java
@@ -1,11 +1,9 @@
 package com.superposition.artist.domain.entity;
 
-import java.util.Date;
 import lombok.Getter;
 
 @Getter
 public class ArtistFollow {
     private String email;
     private String instagramId;
-    private Date createAt;
 }
