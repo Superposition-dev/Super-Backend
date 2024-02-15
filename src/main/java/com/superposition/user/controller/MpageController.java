3730eb297c9cdb1f1665086e76fbc7265837f117
정서현 <tjgus027@kakao.com>
2024-02-15 14:09:17 +0900

    Refactor: import 부분 수정

--- src/main/java/com/superposition/user/controller/MpageController.java
+++ src/main/java/com/superposition/user/controller/MpageController.java
@@ -6,9 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users/me")
