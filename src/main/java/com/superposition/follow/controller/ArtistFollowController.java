af1be6556234423aa0e1a4f4b45045e844ec66ac
harrie8 <hesjs5@naver.com>
2024-02-15 18:35:21 +0900

    Refactor: 작가 팔로우 취소, 작가 팔로우 응답 상태코드 변경

--- src/main/java/com/superposition/follow/controller/ArtistFollowController.java
+++ src/main/java/com/superposition/follow/controller/ArtistFollowController.java
@@ -1,7 +1,7 @@
 package com.superposition.follow.controller;
 
-import com.superposition.follow.dto.ArtistFollowDto;
 import com.superposition.artist.dto.ArtistInfo;
+import com.superposition.follow.dto.ArtistFollowDto;
 import com.superposition.follow.service.ArtistFollowService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -12,25 +12,27 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/artist")
 @RequiredArgsConstructor
 public class ArtistFollowController {
 
     private final ArtistFollowService artistFollowService;
 
-    @GetMapping(value = "/follow")
+    // todo
+    // url 변경하기
+    @GetMapping(value = "/users/artist/follow")
     @ResponseStatus(HttpStatus.OK)
     public List<? extends ArtistInfo> getFollowArtists(@AuthenticationPrincipal UserDetails user) {
         return artistFollowService.getFollowArtistsBy(user.getUsername());
     }
 
-    @PostMapping("/{instagramId}/follow")
-    @ResponseStatus(HttpStatus.OK)
+    // todo
+    // response status 변경하기
+    @PostMapping("/artist/{instagramId}/follow")
+    @ResponseStatus(HttpStatus.CREATED)
     public void followArtist(@AuthenticationPrincipal UserDetails user, @PathVariable String instagramId) {
         ArtistFollowDto dto = ArtistFollowDto.builder()
                 .email(user.getUsername())
@@ -39,7 +41,8 @@ public class ArtistFollowController {
         artistFollowService.followArtist(dto);
     }
 
-    @DeleteMapping("/{instagramId}/follow")
+    @DeleteMapping("/artist/{instagramId}/follow")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteArtistFollow(@AuthenticationPrincipal UserDetails user, @PathVariable String instagramId) {
         ArtistFollowDto dto = ArtistFollowDto.builder()
                 .email(user.getUsername())
