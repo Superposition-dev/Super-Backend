9e41eaab4f24223c4161bfd32f71757410cec2ec
harrie8 <hesjs5@naver.com>
2024-02-21 22:53:51 +0900

    Refactor: 중복된 코드 삭제

--- src/main/java/com/superposition/artist/controller/ArtistController.java
+++ src/main/java/com/superposition/artist/controller/ArtistController.java
@@ -3,11 +3,14 @@ package com.superposition.artist.controller;
 import com.superposition.artist.dto.ResponseArtistDetail;
 import com.superposition.artist.dto.ResponseDisplayArtist;
 import com.superposition.artist.service.ArtistService;
-import com.superposition.user.dto.CurrentUser;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ArtistController {
@@ -19,34 +22,27 @@ public class ArtistController {
 
     @GetMapping("/about")
     @ResponseStatus(HttpStatus.OK)
-    public List<ResponseDisplayArtist> getAboutArtist(){
+    public List<ResponseDisplayArtist> getAboutArtist() {
         return artistService.getAboutArtist();
     }
 
     @GetMapping("/artist")
     @ResponseStatus(HttpStatus.OK)
-    public List<? extends ResponseDisplayArtist> getAllArtist(@RequestParam(value = "search", defaultValue = " ") String search, @RequestParam(value = "isProductPage", defaultValue = "false") boolean isProductPage){
+    public List<? extends ResponseDisplayArtist> getAllArtist(
+            @RequestParam(value = "search", defaultValue = " ") String search,
+            @RequestParam(value = "isProductPage", defaultValue = "false") boolean isProductPage) {
         return artistService.getAllArtist(search.trim(), isProductPage);
     }
 
     @GetMapping("/artist/{instagramId}")
     @ResponseStatus(HttpStatus.OK)
-    public ResponseArtistDetail getArtistByName(@PathVariable String instagramId){
+    public ResponseArtistDetail getArtistByName(@PathVariable String instagramId) {
         return artistService.getArtistInfoById(instagramId);
     }
 
     @PatchMapping("/artist/{instagramId}/view")
     @ResponseStatus(HttpStatus.OK)
-    public void addViewCountByName(@PathVariable String instagramId){
+    public void addViewCountByName(@PathVariable String instagramId) {
         artistService.addViewCountById(instagramId);
-    }
-    @PostMapping("/artist/{instagramId}/follow")
-    @ResponseStatus(HttpStatus.OK)
-    public void followArtist(@CurrentUser String email, @PathVariable String instagramId) {
-        ArtistFollowDto dto = ArtistFollowDto.builder()
-                .email(email)
-                .instagramId(instagramId)
-                .build();
-        artistFollowService.followArtist(dto);
     }
 }
--- src/main/java/com/superposition/user/controller/UserController.java
+++ src/main/java/com/superposition/user/controller/UserController.java
@@ -1,17 +1,21 @@
 package com.superposition.user.controller;
 
-import com.superposition.artist.dto.ArtistInfo;
-import com.superposition.artist.service.ArtistFollowService;
 import com.superposition.user.dto.CurrentUser;
-import com.superposition.user.dto.LoginResponse;
 import com.superposition.user.dto.RequestUserInfo;
 import com.superposition.user.service.UserService;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
@@ -20,39 +24,34 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping(value = "/signup")
-    public ResponseEntity<?> signup(@RequestBody @Valid RequestUserInfo userInfo){
+    public ResponseEntity<?> signup(@RequestBody @Valid RequestUserInfo userInfo) {
         return userService.signup(userInfo);
     }
 
     @GetMapping(value = "/login/kakao")
-    public ResponseEntity<?> kakaoLogin(@RequestParam String code){
+    public ResponseEntity<?> kakaoLogin(@RequestParam String code) {
         return userService.loginByKakao(code);
     }
 
     @GetMapping(value = "/logout")
     @ResponseStatus(HttpStatus.OK)
-    public void logout(@CurrentUser String email){
+    public void logout(@CurrentUser String email) {
         userService.logout(email);
     }
 
     @DeleteMapping
-    public void deleteUser(@CurrentUser String email){
+    public void deleteUser(@CurrentUser String email) {
         userService.deleteUser(email);
     }
 
     @GetMapping(value = "/isAvailable")
     @ResponseStatus(HttpStatus.OK)
-    public Boolean checkUserNickname(@CurrentUser String email){
+    public Boolean checkUserNickname(@CurrentUser String email) {
         return userService.checkNickname(email);
     }
 
     @GetMapping(value = "/regenerateToken")
     public ResponseEntity<?> regenerateToken(@CookieValue("Refresh_Token") String rt) {
         return userService.regenerateToken(rt);
-    }
-    @GetMapping(value = "/artist/follow")
-    @ResponseStatus(HttpStatus.OK)
-    public List<? extends ArtistInfo> getFollowArtists(@CurrentUser String email) {
-        return artistFollowService.getFollowArtistsBy(email);
     }
 }
